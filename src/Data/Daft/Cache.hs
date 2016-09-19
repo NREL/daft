5e7fad71687fee794589b28a7a893d2bbb33646d
Brian W Bush <brian.bush@nrel.gov>
2016-09-19 15:26:45 -0600

    memory-based cache

--- src/Data/Daft/Cache.hs
+++ src/Data/Daft/Cache.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE FlexibleContexts       #-}
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE MultiParamTypeClasses  #-}
 
 
 module Data.Daft.Cache (
   Cache(..)
+, minimum'
+, maximum'
 ) where
 
 
@@ -11,10 +14,33 @@ import Control.Monad.Except (MonadError)
 import Data.Hashable (Hashable)
 
 
-class (Eq o, Hashable o, Ord k, MonadError String m) => Cache o k v m where
+class (Eq o, Hashable o, Ord k, MonadError String m) => Cache o k v m | m -> o, m -> k, m -> v where
   emptyCache :: m ()
   initializeCache :: [o] -> m ()
   eraseCache :: o -> m ()
   clearCache :: o -> m ()
-  lookupPoint :: (o -> k -> Maybe v) -> o -> k -> m v
-  lookupRange :: (o -> Maybe k -> Maybe k -> [(k, v)]) -> o -> Maybe k -> Maybe k -> m [(k, v)]
+  objectCount :: m Int
+  objectList :: m [o]
+  hasNoKeys :: o -> m Bool
+  keysCount :: o -> m Int
+  keysList :: o -> m [k]
+  keysMinimum :: o -> m (Maybe k)
+  keysMaximum :: o -> m (Maybe k)
+  lookupPoint :: (o -> k -> m (Maybe v)) -> o -> k -> m v
+  lookupRange :: (o -> Maybe k -> Maybe k -> m [(k, v)]) -> o -> Maybe k -> Maybe k -> m [(k, v)]
+
+
+minimum' :: Ord a => Maybe a -> Maybe a -> Maybe a
+minimum' x y =
+  do
+    x' <- x
+    y' <- y
+    return $ minimum [x', y']
+
+
+maximum' :: Ord a => Maybe a -> Maybe a -> Maybe a
+maximum' x y =
+  do
+    x' <- x
+    y' <- y
+    return $ maximum [x', y']
--- src/Data/Daft/Cache/Memory.hs
+++ src/Data/Daft/Cache/Memory.hs
@@ -4,28 +4,36 @@
 
 
 module Data.Daft.Cache.Memory (
-  MemoryCacheM(..)
+  Container
+, MemoryCacheT(..)
+, execMemoryCache
 ) where
 
 
 import Control.Arrow ((&&&))
-import Control.Monad.Except (ExceptT, MonadError(..))
-import Control.Monad.State (MonadState(..), State, modify')
-import Data.Daft.Cache (Cache(..))
+import Control.Monad.Except (ExceptT, MonadError(..), MonadIO, runExceptT)
+import Control.Monad.State (MonadState(..), StateT, runStateT, modify')
+import Data.Daft.Cache (Cache(..), maximum', minimum')
 import Data.Hashable (Hashable)
 
 import qualified Data.HashMap.Strict as H
 import qualified Data.Map.Strict as M
 
 
+execMemoryCache :: Monad m => MemoryCacheT o k v m a -> Container o k v -> m (Either String (Container o k v))
+execMemoryCache cache initial =
+  do
+    (x, cache') <- runStateT (runExceptT (runCacheT cache)) initial
+    return $ const cache' <$> x
+
+
 type Container o k v = H.HashMap o (M.Map k v)
 
 
-newtype MemoryCacheM o k v a = MemoryCacheM {runMemoryCacheM :: ExceptT String (State (Container o k v)) a}
-  deriving (Applicative, Functor, MonadError String, Monad, MonadState (Container o k v))
+newtype MemoryCacheT o k v m a = MemoryCacheM {runCacheT :: ExceptT String (StateT (Container o k v) m) a}
+  deriving (Applicative, Functor, Monad, MonadError String, MonadIO, MonadState (Container o k v))
 
-
-instance (Eq o, Hashable o, Ord k) => Cache o k v (MemoryCacheM o k v) where
+instance (Eq o, Hashable o, Ord k, Monad m) => Cache o k v (MemoryCacheT o k v m) where
 
   emptyCache = put H.empty
 
@@ -35,10 +43,58 @@ instance (Eq o, Hashable o, Ord k) => Cache o k v (MemoryCacheM o k v) where
 
   clearCache = modify' . flip H.insert M.empty
 
+  objectCount =
+    do
+      container <- get
+      return $ H.size container
+
+  objectList =
+    do
+      container <- get
+      return $ H.keys container
+
+  hasNoKeys object =
+    do
+      container <- get
+      case H.lookup object container of
+        Nothing    -> return True
+        Just inner -> return $ M.null inner
+
+  keysCount object =
+    do
+      container <- get
+      case H.lookup object container of
+        Nothing    -> return 0
+        Just inner -> return $ M.size inner
+
+  keysList object =
+    do
+      container <- get
+      case H.lookup object container of
+        Nothing    -> return []
+        Just inner -> return $ M.keys inner
+
+  keysMinimum object =
+    do
+      container <- get
+      case H.lookup object container of
+        Nothing    -> return Nothing
+        Just inner -> if M.null inner
+                        then return Nothing
+                        else return . Just . fst $ M.findMin inner
+
+  keysMaximum object =
+    do
+      container <- get
+      case H.lookup object container of
+        Nothing    -> return Nothing
+        Just inner -> if M.null inner
+                        then return Nothing
+                        else return . Just . fst $ M.findMax inner
+
   lookupPoint f object key =
     do
-      let
-        value' = maybe (throwError "MemoryCache: evalution failed.") return $ f object key
+      value' <- maybe (throwError "MemoryCache: evalution failed.") return <$> f object key
       container <- get
       case H.lookup object container of
         Nothing    -> do
@@ -57,54 +113,30 @@ instance (Eq o, Hashable o, Ord k) => Cache o k v (MemoryCacheM o k v) where
       container <- get
       case H.lookup object container of
         Nothing    -> do
-                        let
-                          values = f object lower upper
+                        values <- f object lower upper
                         put $ H.insert object (M.fromList values) container
                         return values
         Just inner -> if M.null inner
                         then do
-                               let
-                                 values = f object lower upper
+                               values <- f object lower upper
                                put $ H.insert object (M.fromList values) container
                                return values
                         else do
                                let
                                  (lower', upper') = (Just . fst . M.findMin &&& Just . fst . M.findMax) inner
                                  (lower'', upper'') = (minimum' lower lower', maximum' upper upper')
-                                 innerLower =
-                                   if lower' == lower''
-                                     then []
-                                     else f object lower'' lower'
-                                 innerUpper =
-                                   if upper' == upper''
-                                     then []
-                                     else f object upper' upper''
+                               innerLower <-
+                                 if lower' == lower''
+                                   then return []
+                                   else f object lower'' lower'
+                               innerUpper <-
+                                 if upper' == upper''
+                                   then return []
+                                   else f object upper' upper''
+                               let
                                  inner' = M.unions [inner, M.fromList innerLower, M.fromList innerUpper]
                                put $ H.insert object inner' container
                                return $ subrange lower upper inner'
-
-
-minimum' :: Ord a => Maybe a -> Maybe a -> Maybe a
-minimum' x y =
-  do
-    x' <- x
-    y' <- y
-    return $ minimum [x', y']
-
-
-maximum' :: Ord a => Maybe a -> Maybe a -> Maybe a
-maximum' x y =
-  do
-    x' <- x
-    y' <- y
-    return $ maximum [x', y']
-
-
-subrange :: Ord k => Maybe k -> Maybe k -> M.Map k v -> [(k, v)]
-subrange Nothing      Nothing      = M.toList                                      
-subrange Nothing      (Just upper) = M.toList .                    splitUpper upper
-subrange (Just lower) Nothing      = M.toList . splitLower lower                   
-subrange (Just lower) (Just upper) = M.toList . splitLower lower . splitUpper upper
 
 
 splitLower :: Ord k => k -> M.Map k v -> M.Map k v
@@ -121,3 +153,10 @@ splitUpper key container =
     (_, center, upper) = M.splitLookup key container
   in
     maybe id (M.insert key) center upper
+
+
+subrange :: Ord k => Maybe k -> Maybe k -> M.Map k v -> [(k, v)]
+subrange Nothing      Nothing      = M.toList                                      
+subrange Nothing      (Just upper) = M.toList .                    splitUpper upper
+subrange (Just lower) Nothing      = M.toList . splitLower lower                   
+subrange (Just lower) (Just upper) = M.toList . splitLower lower . splitUpper upper
